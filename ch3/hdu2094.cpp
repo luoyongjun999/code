cb0ba0ff6b9ba25718e45f47581b3f2a8ab651d8
luoyongjun999 <15512356@qq.com>
2018-11-10 21:59:04 +0800

    code

---
+++ ch3/hdu2094.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    set<string> A, B;                        //定义集合
+    string s1, s2;
+    int n;
+    while(cin >> n && n){
+        for(int i=0; i<n; i++) {
+            cin >> s1 >> s2;
+            A.insert(s1);  A.insert(s2);   //所有人放进集合A
+            B.insert(s2);                     //失败者放进集合B
+        }
+        if(A.size() - B.size() == 1)
+            cout << "Yes" << endl;
+        else
+            cout << "No" << endl;
+        A.clear(); B.clear();
+    }
+    return 0;
+}
---
+++ ch3/hdu2648.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n, m, p;
+    map<string, int> shop;
+    while(cin>>n) {
+        string s;
+        for(int i=1; i<=n; i++) cin>>s; //输入商店名字。实际上用不着处理
+        cin >> m;
+        while(m--) {
+            for(int i=1; i<=n; i++) {
+                cin >> p >> s;
+                shop[s] += p;               //用map可以直接操作商店，加上价格
+            }
+            int rank = 1;
+            map<string,int>::iterator it;           //迭代器
+            for(it=shop.begin(); it != shop.end(); it++)
+                if((*it).second > shop["memory"])   //比较价格
+						rank++;
+            cout<<rank<<endl;
+        }
+        shop.clear();
+    }
+    return 0;
+}
