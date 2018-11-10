42722275fc81234f7babda5d0a4e8ee7cead5bf3
luoyongjun999 <15512356@qq.com>
2018-11-10 21:41:22 +0800

    code

---
+++ ch3/hdu1062.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+		int n;
+		char ch;
+		scanf("%d",&n);  getchar();
+		while(n--){
+			stack<char> s;
+			while(true){
+				ch = getchar();                   //一次读入一个字符
+            	if(ch==' '||ch=='\n'||ch==EOF){
+					while(!s.empty()){
+						printf("%c",s.top());  //输出栈顶
+						s.pop();                  //清除栈顶
+					}
+					if(ch=='\n'||ch==EOF)  break;
+					printf(" ");
+				}
+				else   s.push(ch);                //入栈
+			}
+			printf("\n");
+		}
+	return 0;
+}
---
+++ ch3/hdu1702.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t,n,temp;
+    cin>>t;
+    while(t--){
+        string str,str1;
+        queue<int>Q;
+        stack<int>S;
+        cin>>n>>str;
+        for(int i=0; i<n; i++){
+            if(str=="FIFO"){                  //¶ÓÁÐ
+                cin>>str1;
+                if(str1=="IN"){
+                    cin>>temp;  Q.push(temp);
+                }
+                if(str1=="OUT"){
+                    if(Q.empty()) cout<<"None"<<endl;
+                    else{
+                        cout<<Q.front()<<endl;
+                        Q.pop();
+                    }
+                }
+            }
+            else{                                //Õ»
+                cin>>str1;
+                if(str1=="IN"){
+                    cin>>temp;  S.push(temp);
+                }
+                if(str1=="OUT"){
+                    if(S.empty()) cout<<"None"<<endl;
+                    else {
+                        cout<<S.top()<<endl;
+                        S.pop();
+                    }
+                }
+            }
+        }
+    }
+    return 0;
+}
