f3e9b62426e04433d177fefe7a36ba46bc55a4c1
luoyongjun999 <15512356@qq.com>
2019-04-30 15:32:34 +0800

    430

--- ch8/hdu1028.cpp
+++ ch8/hdu1028.cpp
@@ -3,34 +3,28 @@
 using namespace std;
 const int MAXN=200;
 int c1[MAXN+1],c2[MAXN+1];
-void part()
-{
+void part(){
     int i,j,k;
-    for(i=0;i<=MAXN;i++)
-    {   c1[i]=1;
+    for(i=0;i<=MAXN;i++){
+        c1[i]=1;
         c2[i]=0;
     }
-    for(k=2;k<=MAXN;k++)
-    {
+    for(k=2;k<=MAXN;k++){
       for(i=0;i<=MAXN;i++)
         for(j=0;j+i<=MAXN;j+=k)
-        {
-            c2[i+j]+=c1[i];
-        }
-      for(i=0;i<=MAXN;i++)
-      {
+           c2[i+j]+=c1[i];
+
+      for(i=0;i<=MAXN;i++){
           c1[i]=c2[i];
           c2[i]=0;
       }
     }
 }
-int main()
-{
+int main(){
     int n;
     part();
     while(cin>>n)
-    {
-        cout<<c1[n]<<endl;
-    }
+      cout<<c1[n]<<endl;
+
     return 0;
 }
